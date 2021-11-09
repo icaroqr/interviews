b797eed36e3b58b7ae57f07853503b81957443a6
Icaro Rezende <icarorez@gmail.com>
2021-11-09 11:50:49 -0300

    Melhorando algoritmo

--- src/main/java/com/interviews/hexacta/questionTest1/MenorInt.java
+++ src/main/java/com/interviews/hexacta/questionTest1/MenorInt.java
@@ -1,4 +1,7 @@
 package com.interviews.hexacta.questionTest1;
+
+import java.util.Arrays;
+
 /**
  * A classe {@code MenorInt} possui métodos que auxiliam na busca dentro de uma lista de números inteiros,
  * pelo menor número inteiro que não existe dentro dela
@@ -7,43 +10,24 @@ package com.interviews.hexacta.questionTest1;
 public class MenorInt {
 
     /**
-     * Método recursivo que recebe uma lista de números inteiros e executa regras necessárias
+     * Método que recebe uma lista de números inteiros, ordena de forma ascendente e itera a mesma
      * para retornar o menor número inteiro que não existe dentro dela 
      * @param   lista   lista de números inteiros
      * @return  menor número inteiro não existente na lista {@code lista} recebida
      */
-    public int solucao(int[] lista) {
-        //Encontra o menor inteiro da colecao
-        int menorEncontrado = retornaMenorInteiroDaLista(lista);
-        int proximoInteiro = menorEncontrado + 1;
-            //Verifica se o proximo inteiro existe na colecao
-            if(isNumeroNaLista(proximoInteiro,lista) && lista != null){
-                //Se existe, retira o numero e chama a mesma função recursivamente
-                lista = retiraNumeroDaLista(menorEncontrado,lista);
-                return solucao(lista);
-            }else{
-                //Se não existe, nula a lista como condição de parada e retorna o valor encontrado
-                lista = null;
-                if(proximoInteiro < 0){
-                    proximoInteiro = 1;
+    public int solucao(int[] lista){
+        Arrays.sort(lista);
+        for(int i = 0; i < lista.length; i++){
+            int proximo = lista[i] + 1;
+                if(!isNumeroNaLista(proximo, lista)){
+                    if(proximo < 0){
+                        return 1;
+                    }else{
+                        return proximo;
+                    }
                 }
-                return proximoInteiro;
-            }
-    }
-    
-    /**
-     * Método que retorna o menor número inteiro de uma lista de números inteiros
-     * @param   lista   lista de números inteiros
-     * @return  menor número inteiro da lista {@code lista} recebida
-     */
-    private int retornaMenorInteiroDaLista (int[] lista) {
-	    int menorNumero=lista[0];
-	    for(int i=0; i<lista.length; i++){
-	        if(lista[i] < menorNumero){
-	            menorNumero = lista[i];
-	        }
-	    }
-	    return menorNumero;
+        }
+        return 0;
     }
     
     /**
@@ -59,33 +43,6 @@ public class MenorInt {
 	        }
 	    }
 	    return false;
-    }
-    
-    /**
-     * Método que recebe uma lista e um número para ser retirado dela, retornando uma cópia da lista sem o número
-     * recebido por parâmetro
-     * @param   numero   numero que deseja retirar
-     * @param   lista   lista de números inteiros de onde o número será retirado
-     * @return  retorna uma cópia da lista {@code lista} sem o número recebido {@code numero}
-     */
-     private int[] retiraNumeroDaLista(int numero, int[] lista){
-        //Encontra quantas vezes o numero aparece na lista
-        int nuOcorrenciasNumero = 0;
-        for(int i=0; i<lista.length; i++){
-	        if(lista[i] == numero){
-               nuOcorrenciasNumero++;
-	        }
-	    }
-        //Cria novo array baseado na ocorrencia do numero e faz uma cópia
-        int indexNovo = 0;
-        int[] listaNova = new int[lista.length - nuOcorrenciasNumero];
-        for(int i=0; i<lista.length; i++){
-	        if(lista[i] != numero){
-	            listaNova[indexNovo] = lista[i];
-                indexNovo++;
-	        }
-	    }
-        return listaNova;
     }
     
 }
